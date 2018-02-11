329ed8f421e7a0b597473202d78084757ad43442
Toni Granados <toni@tngranados.com>
2018-02-11 19:30:06 +0100

    Add license and readme with assets attributions

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 -- Configuration
 function love.conf(t)
-    t.title = "Flappy"
+    t.title = "Bored Dragon in a Cave"
     t.version = "0.10.2"
     t.window.width = 1280
     t.window.height = 720
