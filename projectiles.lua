a205370ecee216f6648a9b0c3b06b92189308b52
Toni Granados <toni@tngranados.com>
2018-02-11 17:09:29 +0100

    Add overheating mechanic to player

--- projectiles.lua
+++ projectiles.lua
@@ -7,11 +7,18 @@ function projectiles.update(dt)
     player.shootTimer = player.shootTimer - dt
     if player.shootTimer <= 0 then
         player.canShoot = true
+        player.shootTimer = player.shootTimerMax
+    end
+    player.shootLimitTimer = player.shootLimitTimer - dt
+    if player.shootLimitTimer <= 0 and player.shootLimit > 0 then
+        player.shootLimit = player.shootLimit - 1
+        player.shootLimitTimer = player.shootLimitTimerMax
     end
 
-    if love.keyboard.isDown("space", "rctrl", "lctrl", "ctrl") and player.canShoot then
+
+    if love.keyboard.isDown("space", "rctrl", "lctrl", "ctrl") and player.canShoot and player.shootLimit < player.shootLimitMax  then
         player.canShoot = false
-        player.shootTimer = player.shootTimerMax
+        player.shootLimit = player.shootLimit + 1
 
         newProjectile = {
             x = player.x + player.img:getWidth() / 1.2,
